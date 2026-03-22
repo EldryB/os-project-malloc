825958cf67eda413054418551fc5d19eb084a70a
Eldry <eldryjesu@gmail.com>
2026-03-22 15:18:39 -0400

    Funcion my_malloc completada

--- src/mm_malloc.c
+++ src/mm_malloc.c
@@ -3,14 +3,91 @@
 #include "mm_malloc.h"
 
 // Inicio de la lista enlazada del heap
-void *base = NULL;
+struct block_meta *base = NULL;//Base block_meta
 
-void *my_malloc(size_t size) {
-    // TODO: Implementar First-Fit o Best-Fit
-    // 1. Verificar si hay un bloque libre del tamaño adecuado.
-    // 2. Si no, pedir espacio al OS con sbrk().
-    return NULL; 
+struct block_meta *request_mem(struct block_meta *last, size_t size)
+{
+    struct block_meta* block;
+
+    void *request = sbrk(size + sizeof(struct block_meta));//Le pedimos memoria al SO
+
+    if(request == (void*) -1)
+    {
+        return NULL;//Si me rechaza la peticion retornamos null
+    }
+
+    block = (struct block_meta *)request;//Le asignamos la memoria
+    
+    block->size = size;//Asignamos el tamanho
+    block->next = NULL;
+    block->free = 0;
+
+    if(last)
+    {
+        last->next = block;
+    }
+
+    return block;
 }
+
+struct block_meta *find_free_block(struct block_meta **last, size_t size) 
+{
+    struct block_meta *current = *last; 
+    while (current != NULL) //Recorremos toda la lista hasta encontrar un bloque libre
+    {
+    
+        if (current->free == 1 && current->size >= size) 
+        {
+            return current;
+        }
+        *last = current; 
+        current = current->next;
+    }
+    return NULL; //Si no lo encontramos retornamos NULL
+}
+
+void *my_malloc(size_t size) 
+{
+    struct block_meta *block;
+
+    if(size <= 0)
+    {
+        return NULL;
+    }
+
+    if(base == NULL)//Caso base: cuando base = Null
+    { 
+        block = request_mem(NULL, size);//Pedimos memoria
+        if(block == NULL)
+        {
+            return NULL;//Si niegan la peticion retornamos null
+        }
+        base = block;//Le asignamos block a base para tomarla como inicio de la lista
+    }
+
+    else {
+        struct block_meta *last = base;
+
+        block = find_free_block(&last, size);//Buscams un bloque libre
+        
+        if(block == NULL) //Si no lo encontramos pedimos mas memoria
+        {
+            block = request_mem(last, size);
+            if(block == NULL) 
+            {
+                return NULL;//Si nos niegan la peticion, retornamos null.
+            }
+        } 
+        else 
+        {
+            block->free = 0; //Si lo encontramos, lo ocupamos
+        }
+    }
+
+    void *user_data = (void *)(block + 1);//Le retornamos al usuario el lugar
+    return user_data;//inmediatamente siguiente para no darle acceso y pueda cambiar nuestra meta_data
+}
+
 
 void my_free(void *ptr) {
     // TODO: Marcar el bloque como libre.
